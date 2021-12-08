47fad710c5fa4d03cd73f41bbfdf2116cfa363f2
MzamoMahaeng8310 <81557081+MzamoMahaeng8310@users.noreply.github.com>
2021-12-08 12:05:21 +0200

    Add project files.

---
+++ SeleniumCSharpNetCore/CustomControl.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCSharpNetCore
+{
+    public class CustomControl : DriverHelper
+    {
+
+        public static void ComboBox(String controlName, String ValueToSelect)
+        {
+            //The combobox
+            IWebElement cmbxAllFoods = Driver.FindElement(By.XPath($"//input[@id='{controlName}-awed']"));
+            //C# striing Interpolation is $ and {} enter the variable name
+            cmbxAllFoods.Clear();
+            cmbxAllFoods.SendKeys(ValueToSelect);
+            //Click the item in the combobox
+            Driver.FindElement(By.XPath($"//div[@id='{controlName}-dropmenu']//li[text()='{ValueToSelect}']")).Click();
+        }
+
+
+
+    }
+}
---
+++ SeleniumCSharpNetCore/DriverHelper.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCSharpNetCore
+{
+    public  class DriverHelper
+    {
+        //prop and double tab
+        //it will bring up the get set properties 
+
+        public static IWebDriver Driver { get; set; }
+    }
+}
